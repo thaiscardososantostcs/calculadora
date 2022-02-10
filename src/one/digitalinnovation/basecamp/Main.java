c6b4d3f4aa9230a00bf44ceec6cf649bfdd0b345
Thais Cardoso Santos <thaiscardososantos.98@gmail.com>
2022-02-10 15:34:19 -0300

    Initial commit

---
+++ src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercicio de Calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(4, 2);
+        Calculadora.multiplicacao(2, 1);
+        Calculadora.divisao(8, 2);
+
+    }
+    
+}
